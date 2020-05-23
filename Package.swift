288d616c3efa83ca24a2114295be1dbf72925d13
Hans Ospina <hansospina@gmail.com>
2020-05-23 06:16:42 -0500

    updates for spm-xcode

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         name: "SBX",
         
         products: [
-            .library(name: "SBX", targets: ["SBX"]),
+            .library(name: "sbxcloudswift", targets: ["sbxcloudswift"]),
         ],
         dependencies: [
         ],
@@ -15,13 +15,13 @@ let package = Package(
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages which this package depends on.
             .target(
-                    name: "SBX",
+                    name: "sbxcloudswift",
                     dependencies: [],
                     path: "Sources"
             ),
             .testTarget(
                     name: "SBXTests",
-                    dependencies: ["SBX"]
+                    dependencies: ["sbxcloudswift"]
             )
         ]
 )
